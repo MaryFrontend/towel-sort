36d844b124f114d369765fe78b4d998a7581c070
MaryFrontend <4rontend@gmail.com>
2021-03-14 10:51:10 +0300

    feat:complete

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || matrix.length === 0){
+        return[]
+    }
+
+    let arr = [] ;
+
+    for(let i = 0; i < matrix.length; i++) {
+        if(i % 2 === 0) {
+            arr.push(matrix[i]);
+        } else {
+            arr.push(matrix[i].reverse());
+        }
+    }
+matrix =arr.flat()
+    return (matrix)// венуть новый массив
 }
